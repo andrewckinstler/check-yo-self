2aa5c1ef0874e55c55d9cb9758a3c8e876c4fe6f
sertmer <scottertmer@gmail.com>
2019-09-03 21:01:48 -0600

    Add psuedo code to create task list func

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var makeTaskBtn = document.querySelector('.make-task');
 
 makeTaskBtn.addEventListener('click', createTaskList);
 
+
+// input: click
+// output: create new task list card
+// user will add task title to input, and task items to input
+// when they click make task list button, the title and individual tasks
+// will be added to a new card in the main section, spilling
+// from top left, to top right, and so on.
 function createTaskList() {
-  console.log('does it work?')
+  console.log('does it work?');
 }
