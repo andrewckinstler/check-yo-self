cfafc3e927725c8ddcc4d3f756aec32ff7594623
sertmer <scottertmer@gmail.com>
2019-09-03 20:56:02 -0600

    Add components for Make-task-button

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var makeTaskBtn = document.querySelector('.make-task')
+var makeTaskBtn = document.querySelector('.make-task');
+
+makeTaskBtn.addEventListener('click', createTaskList);
+
+function createTaskList() {
+  console.log('does it work?')
+}
