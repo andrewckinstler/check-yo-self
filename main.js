a749cf77eb23d8948bf3d41f161d891d416425db
sertmer <scottertmer@gmail.com>
2019-09-04 11:01:28 -0600

    Modify create task list function to include the user-made task items and task title.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var taskInput = document.querySelector('.form-item');
 var makeTaskBtn = document.querySelector('.make-list');
 var mainSection = document.querySelector('.main');
 var taskTitle = document.querySelector('.title');
+var taskList = document.querySelector('.task-list')
 
 addTasks.addEventListener('click', appendTask);
 makeTaskBtn.addEventListener('click', createTaskList);
@@ -21,14 +22,13 @@ document.querySelectorAll('.form').forEach(function(form) {
 function createTaskList() {
   console.log('does it work?');
   mainSection.innerHTML +=
+  // taskList.innerHTML;
   `<article class="task-card">
     <div class="divider divider-top">
     <h4>${taskTitle.value}</h4>
     </div>
     <ul>
-      <li>${taskInput.value}</li>
-      <li>${taskInput.value}</li>
-      <li>${taskInput.value}</li>
+      ${taskList.innerHTML}
     </ul>
     <div class="divider">
     <input type="image" class="urgent-button" src="check-yo-self-icons/urgent.svg"/>
@@ -42,10 +42,7 @@ function createTaskList() {
 // 2 clear fields
 // 3 disable button until keydown
 
-// function disableButton() {
-//   makeTaskBtn.classList.add('disabled-button');
-//   makeTaskBtn.disabled = true;
-//   }
+
 
 function appendTask() {
   taskList.innerHTML += `<li><button class="delete-button">X</button>${taskInput.value}</li>`
