e46c362f0feaaa1e33fb64b3404c13ed7f49dd84
sertmer <scottertmer@gmail.com>
2019-09-04 11:05:50 -0600

    Close open div in createTaskList func

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var taskTitle = document.querySelector('.title');
 var taskList = document.querySelector('.task-list')
 
 addTasks.addEventListener('click', appendTask);
-makeTaskBtn.addEventListener('click', createTaskList);
+makeTaskBtn.addEventListener('click', taskListButtonHandler);
 document.querySelectorAll('.form').forEach(function(form) {
   form.addEventListener('keyup', disableClear);
 });
@@ -19,26 +19,33 @@ document.querySelectorAll('.form').forEach(function(form) {
 // when they click make task list button, the title and individual tasks
 // will be added to a new card in the main section, spilling
 // from top left, to top right, and so on.
+
+
+function taskListButtonHandler() {
+  createTaskList();
+}
+
 function createTaskList() {
   console.log('does it work?');
   mainSection.innerHTML +=
   // taskList.innerHTML;
   `<article class="task-card">
     <div class="divider divider-top">
-    <h4>${taskTitle.value}</h4>
+      <h4>${taskTitle.value}</h4>
     </div>
     <ul>
       ${taskList.innerHTML}
     </ul>
     <div class="divider">
     <input type="image" class="urgent-button" src="check-yo-self-icons/urgent.svg"/>
-    <input type="image" class="delete-button" src="check-yo-self-icons/delete.svg"/>`
+    <input type="image" class="delete-button" src="check-yo-self-icons/delete.svg"/>
+    </div>`
 };
 
 // Ertmer's pseudo code for click on make task list button
 // input: click,
 // output: several funcs, wrapped up in button handler function
-// 1 create new card (create task list func)
+// 1 create new card (create task list func) DUNZO
 // 2 clear fields
 // 3 disable button until keydown
 
