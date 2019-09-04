ada066a8a1e333fde6dcb08882193fc82d97344b
Ben Firth <bfirth93@gmail.com>
2019-09-04 17:32:39 -0600

    Added todo-list skeleton

---
+++ todo-list.js
@@ -0,0 +1,22 @@
+class ToDoList {
+  constructor(id,titleString,trueOrFalse) {
+    this.id = id;
+    this.title = titleString;
+    this.urgent = trueOrFalse;
+    this.tasks = [];
+  };
+  saveToStorage() {
+
+  };
+  deleteFromStorage() {
+
+  };
+  updateToDo() {
+
+  };
+  updateTask() {
+
+  };
+};
+
+module.exports = ToDoList;
