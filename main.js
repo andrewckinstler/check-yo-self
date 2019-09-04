8ecbecc04f6ecfbba2f9b2ff8b8559d4a9bb7e70
Andrew Kinstler <andrewckinstler@gmail.com>
2019-09-04 09:08:53 -0600

    Added button functionality

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-var taskList = document.querySelector('.task-list')
-var addTasks = document.querySelector('.add-task')
+var taskList = document.querySelector('.task-list');
+var addTasks = document.querySelector('.add-task');
+var taskInput = document.querySelector('.form-item');
+
+addTasks.addEventListener('click', appendTask);
+
+function appendTask() {
+  taskList.innerHTML += `<li><button class="delete-button">X</button>${taskInput.value}</li>`
+};
